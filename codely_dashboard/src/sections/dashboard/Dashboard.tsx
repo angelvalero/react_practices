c79783aeb985760cf908c46b2ceb5eb9cd44326e
angelvalero <angelvapa8@gmail.com>
2024-02-27 11:07:54 +0100

    Refactorize code part4

--- codely_dashboard/src/sections/dashboard/Dashboard.tsx
+++ codely_dashboard/src/sections/dashboard/Dashboard.tsx
@@ -1,41 +1,14 @@
 import { useEffect, useState } from "react";
 
 import { config } from "../../devdash_config";
-import { GitHubApiGitHubRepositoryRepository } from "../../infrastructure/GitHubApiGitHubRepositoryRepository";
-import { GitHubApiResponses } from "../../infrastructure/GitHubApiResponse";
+import { GitHubRepository } from "../../domain/GitHubRepository";
+import { GitHubRepositoryRepository } from "../../domain/GitHubRepositoryRepository";
 import Brand from "./brand.svg?react";
-import Check from "./check.svg?react";
 import styles from "./Dashboard.module.scss";
-import Error from "./error.svg?react";
-import PullRequests from "./git-pull-request.svg?react";
-import IssueOpened from "./issue-opened.svg?react";
-import Lock from "./lock.svg?react";
-import Forks from "./repo-forked.svg?react";
-import Start from "./star.svg?react";
-import Unlock from "./unlock.svg?react";
-import Watchers from "./watchers.svg?react";
+import { GitHubRepositoryWidget } from "./GitHubRepositoryWidget";
 
-const isoToReadableDate = (lastUpdate: string): string => {
-	const lastUpdateDate = new Date(lastUpdate);
-	const currentDate = new Date();
-	const diffTime = currentDate.getTime() - lastUpdateDate.getTime();
-	const diffDays = Math.round(diffTime / (1000 * 3600 * 24));
-
-	if (diffDays === 0) {
-		return "today";
-	}
-
-	if (diffDays > 30) {
-		return "more than a month ago";
-	}
-
-	return `${diffDays} days ago`;
-};
-
-const repository = new GitHubApiGitHubRepositoryRepository(config.github_access_token);
-
-export function Dashboard() {
-	const [repositoryData, setRepositoryData] = useState<GitHubApiResponses[]>([]);
+export function Dashboard({ repository }: { repository: GitHubRepositoryRepository }) {
+	const [repositoryData, setRepositoryData] = useState<GitHubRepository[]>([]);
 
 	useEffect(() => {
 		repository
@@ -43,7 +16,7 @@ export function Dashboard() {
 			.then((repositoryData) => {
 				setRepositoryData(repositoryData);
 			});
-	}, []);
+	}, [repository]);
 
 	return (
 		<>
@@ -53,61 +26,20 @@ export function Dashboard() {
 					<h1 className={styles.app__brand}>DevDash_</h1>
 				</section>
 			</header>
-			<section className={styles.container}>
-				{repositoryData.map((widget) => (
-					<article className={styles.widget} key={widget.repositoryData.id}>
-						<header className={styles.widget__header}>
-							<a
-								className={styles.widget__title}
-								href={widget.repositoryData.html_url}
-								target="_blank"
-								title={`${widget.repositoryData.organization.login}/${widget.repositoryData.name}`}
-								rel="noreferrer"
-							>
-								{widget.repositoryData.organization.login}/{widget.repositoryData.name}
-							</a>
-							{widget.repositoryData.private ? <Lock /> : <Unlock />}
-						</header>
-						<div className={styles.widget__body}>
-							<div className={styles.widget__status}>
-								<p>Last update {isoToReadableDate(widget.repositoryData.updated_at)}</p>
-								{widget.ciStatus.workflow_runs.length > 0 && (
-									<div>
-										{widget.ciStatus.workflow_runs[0].status === "completed" ? (
-											<Check />
-										) : (
-											<Error />
-										)}
-									</div>
-								)}
-							</div>
-							<p className={styles.widget__description}>{widget.repositoryData.description}</p>
-						</div>
-						<footer className={styles.widget__footer}>
-							<div className={styles.widget__stat}>
-								<Start />
-								<span>{widget.repositoryData.stargazers_count}</span>
-							</div>
-							<div className={styles.widget__stat}>
-								<Watchers />
-								<span>{widget.repositoryData.watchers_count}</span>
-							</div>
-							<div className={styles.widget__stat}>
-								<Forks />
-								<span>{widget.repositoryData.forks_count}</span>
-							</div>
-							<div className={styles.widget__stat}>
-								<IssueOpened />
-								<span>{widget.repositoryData.open_issues_count}</span>
-							</div>
-							<div className={styles.widget__stat}>
-								<PullRequests />
-								<span>{widget.pullRequests.length}</span>
-							</div>
-						</footer>
-					</article>
-				))}
-			</section>
+			{repositoryData.length === 0 ? (
+				<div className={styles.empty}>
+					<span>No hay widgets configurados.</span>
+				</div>
+			) : (
+				<section className={styles.container}>
+					{repositoryData.map((repository) => (
+						<GitHubRepositoryWidget
+							key={`${repository.id.organization}/${repository.id.name}`}
+							repository={repository}
+						/>
+					))}
+				</section>
+			)}
 		</>
 	);
 }
--- codely_dashboard/src/sections/dashboard/devdash_config.ts
+++ codely_dashboard/src/sections/dashboard/devdash_config.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 export interface DevDashConfig {
   github_access_token: string;
   widgets: {
@@ -8,7 +7,7 @@ export interface DevDashConfig {
 }
 
 export const config: DevDashConfig = {
-  github_access_token: import.meta.env.VITE_APP_GITHUB_PERSONAL_ACCESS_TOKEN,
+  github_access_token: process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN as string,
   widgets: [
     {
       id: "2565fa91-2ac4-4e4f-9111-6d27a598082d",
