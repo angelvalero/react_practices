c50b7209a04fc442ce7c34dfc9c7f788768fd1cd
angelvalero <angelvapa8@gmail.com>
2024-02-29 12:50:52 +0100

    Solving

--- codely_dashboard/src/infrastructure/LocalStorageGithubAccessTokenRepository.ts
+++ codely_dashboard/src/infrastructure/LocalStorageGithubAccessTokenRepository.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import { GitHubAccessTokenRepository } from "../domain/GitHubAccessTokenRepository";
 
 export class LocalStorageGitHubAccessTokenRepository implements GitHubAccessTokenRepository {
